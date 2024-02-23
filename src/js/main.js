a161e93f8f7fdbe62e412775ec80a2ef7db94bfe
Alexander Hilding <hildingx@gmail.com>
2024-02-23 08:29:56 +0100

    Provar dölja API-nycklarna med dotenv-paketet.

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
+const nytApiKey = process.env.NYT_API_KEY;
+const tmdbApiKey = process.env.TMDB_API_KEY;
+
 async function fetchReviewData() {
     try {
-        const response = await fetch("https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=section_name:%22Movies%22%20AND%20type_of_material:%22Review%22&api-key=qg2eTClj7amxRfzreMN776WOGElnByzB");
+        const response = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=section_name:"Movies"%20AND%20type_of_material:"Review"&api-key=${process.env.NYT_API_KEY}`);
         const data = await response.json();
 
         console.log(data);
@@ -9,19 +13,11 @@ async function fetchReviewData() {
     }
 }
 
-//fetchData();
-
-const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YTEyMGYwMjYyNTA1NzRlMDYyNzU1ODhhMzJmZGVjYSIsInN1YiI6IjY1ZDcxZGY4YjA0NjA1MDE3YjBhOTY4ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4M-n0rzAklAIF5RHUNtrE0zQigSAUy7gzojZRfKGj20'
-    }
-  };
+//fetchReviewData();
 
 async function fetchMovieData() {
     try {
-        const response = await fetch('https://api.themoviedb.org/3/authentication', options);
+        const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=harry&api_key=${process.env.TMDB_API_KEY}&append_to_response=credits`);
         const data = await response.json();
 
         console.log(data);
@@ -31,3 +27,4 @@ async function fetchMovieData() {
 }
 
 //fetchMovieData();
+
