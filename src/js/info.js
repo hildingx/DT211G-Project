ff664335c2a1809881a05e55763d094f3f5a14d2
Alexander Hilding <hildingx@gmail.com>
2024-02-28 10:58:43 +0100

    Lagt till styles med sass. Förfinat sökningen ytterligare med fler sökparametrar hämtade från tmdb till ny times api.
    Lagt till "top 3 picks" på sidan.

---
+++ src/js/info.js
@@ -0,0 +1,7 @@
+//Öppna ruta vid klick på ?
+document.getElementById('info-trigger').addEventListener('click', function() {
+  let infoContent = document.getElementById('info-content');
+  let expanded = this.getAttribute('aria-expanded') === 'true';
+  this.setAttribute('aria-expanded', !expanded);
+  infoContent.style.display = expanded ? 'none' : 'block';
+});
--- src/js/main.js
+++ src/js/main.js
@@ -18,14 +18,14 @@ const mockData = {
     ],
 };
 
-let useMock = true;
+let useMock = false;
 
 async function fetchMovieDataAndReview(searchQuery) {
     if (useMock) {
         return mockData.results[0];
     } else {
             try {
-                const tmdbResponse = await fetch(`https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(searchQuery)}&api_key=${tmdbApiKey}&append_to_response=credits`);
+                const tmdbResponse = await fetch(`https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(searchQuery)}&api_key=${tmdbApiKey}`);
                 const tmdbData = await tmdbResponse.json();
 
                 if (tmdbData.results.length > 0) {
@@ -35,20 +35,36 @@ async function fetchMovieDataAndReview(searchQuery) {
                         movieTitle: tmdbData.results[0].title,
                         releaseDate: tmdbData.results[0].release_date,
                         moviePoster: tmdbData.results[0].poster_path,
+                        movieOverview: tmdbData.results[0].overview,
                     };
+                    
+                    const movieId = tmdbData.results[0].id;
+                    console.log("movie id " + movieId);
+
+                    
+                    const tmdbDataExtendedResponse = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${tmdbApiKey}&language=en-US&append_to_response=credits`);
+                    const tmdbDataExtended = await tmdbDataExtendedResponse.json();
+
+                    let runTimeMinutes = tmdbDataExtended.runtime;
+                    let hours = Math.floor(runTimeMinutes / 60);
+                    let minutes = runTimeMinutes % 60;
+
+                    movieData.runTime = `${hours}h ${minutes}m`;                
+
+                    const director = tmdbDataExtended.credits.crew.find(person => person.job === "Director");
+                    const directorName = director.name;
+                    console.log("director " + directorName);
 
                     const releaseYear = movieData.releaseDate.split('-')[0];
 
                     if (releaseYear) {
-                        const nytResponse = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${encodeURIComponent(searchQuery)}&fq=section_name:"Movies"%20AND%20type_of_material:"Review"%20AND%20pub_year:"${releaseYear}"&api-key=${nytApiKey}`);
+                        const nytResponse = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${encodeURIComponent(searchQuery + " " + directorName)}&fq=section_name:"Movies"%20AND%20type_of_material:"Review"%20AND%20pub_year:"${releaseYear}"&api-key=${nytApiKey}`);
                         const nytData = await nytResponse.json();
 
                         if (nytData.response && nytData.response.docs.length > 0) {
-                            console.log(nytData.response.docs[0].headline.main);
-
-                            movieData.reviewHeadline = nytData.response.docs[0].headline.main;
                             movieData.reviewUrl = nytData.response.docs[0].web_url;
                             movieData.reviewAbstract = nytData.response.docs[0].abstract;
+                            movieData.headlineMain = nytData.response.docs[0].headline.main;
                             movieData.reviewLeadParagraph = nytData.response.docs[0].lead_paragraph;
                             movieData.reviewer = nytData.response.docs[0].byline.original.replace("By ", "");
                         } else {
@@ -75,35 +91,78 @@ function updateDOMWithMovieData(movieData) {
     if (movieData) {
         let movieHtml = `
         <div class="movie">
-            <p>${movieData.movieTitle}</p>
-            <p>${movieData.releaseDate}</p>
-            <img src="https://image.tmdb.org/t/p/w500/${movieData.moviePoster}" alt="Filmposter av ${movieData.movieTitle}" class="movie-poster">
+            <div class="tmdb">
+                <img src="https://image.tmdb.org/t/p/w500/${movieData.moviePoster}" alt="Filmposter av ${movieData.movieTitle}" class="movie-poster">
+                <div class="tmdb-info">
+                    <h2>${movieData.movieTitle}</h2>
+                    <h3>${movieData.releaseDate.split('-')[0]} • ${movieData.runTime}</h3>
+                    <p>${movieData.movieOverview}</p>
+                    <p>Data provided by:</p>
+                    <a href="https://www.themoviedb.org/">
+                        <img src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg" alt="Data provided by The Movie Database">
+                    </a>
+                </div>
+            </div>
         `;
 
-        if (movieData.reviewHeadline) {
+        const wordCount = movieData.reviewAbstract ? movieData.reviewAbstract.split(' ').length : 0;
+        const reviewText = wordCount > 20 ? movieData.headlineMain : movieData.reviewAbstract;
+
+        if (movieData.reviewAbstract) {
             movieHtml += `
-                <p>${movieData.reviewHeadline}</p>
-                <p>${movieData.reviewLeadParagraph}</p>
-                <p>Av: ${movieData.reviewer}</p>
-                <p>Läs hela recensionen på <a href="${movieData.reviewUrl}">NYT</a></p>
+                <div class="nyt">
+                    <blockquote>
+                        <h2>${reviewText}</h2>
+                        <p>${movieData.reviewLeadParagraph}</p>
+                        <cite>- ${movieData.reviewer}</cite>
+                    </blockquote>
+                    <p>Läs hela recensionen på <a href="${movieData.reviewUrl}">New York Times.</a></p>
+                    <a href="https://developer.nytimes.com/">
+                        <img src="https://developer.nytimes.com/files/poweredby_nytimes_200c.png?v=1583354208354" alt="Data provided by New York Times-logo">
+                    </a>
+                </div>
             `;
         } else {
-            movieHtml += `<p>Ingen recension hittades.</p>`;
+            movieHtml += `<p class="no-review">Ingen recension hittades.</p>`;
         }
 
         movieHtml += `</div>`;
         moviesEl.innerHTML = movieHtml;
     } else {
-        moviesEl.innerHTML = "<p>Ingen filmdata hittades.</p>";
+        moviesEl.innerHTML = `<p class="no-data">Inga filmer hittades med den angivna söktermen.</p>`;
     }
 }
 
 
 document.getElementById('search-form').addEventListener('submit', async function(event) {
     event.preventDefault();
+    this.classList.add('form-moved');
+    document.body.classList.add('body-bg');
+    document.querySelector('h1').classList.add('green-color');
+    document.querySelector('.blink-cursor').classList.add('green-cursor');
+    document.querySelector('#info-trigger').classList.add('info-trigger-white');
+    document.getElementById('movies').classList.add('movies-styles');
+    document.getElementById('favorites').style.display = 'none';
+    const searchInput = document.getElementById('search-box');
     const searchQuery = document.getElementById('search-box').value;
+    searchInput.placeholder = "Sök efter en ny film...";
     const data = await fetchMovieDataAndReview(searchQuery);
     updateDOMWithMovieData(data);
 })
 
 
+document.querySelectorAll('.movie-pick').forEach(pick => {
+    pick.addEventListener('click', function() {
+        const movieTitle = this.querySelector('.movie-title').textContent;
+        fetchMovieDataAndReview(movieTitle).then(data => {
+            document.getElementById('search-form').classList.add('form-moved');
+            document.body.classList.add('body-bg');
+            document.querySelector('h1').classList.add('green-color');
+            document.querySelector('.blink-cursor').classList.add('green-cursor');
+            document.querySelector('#info-trigger').classList.add('info-trigger-white');
+            document.getElementById('movies').classList.add('movies-styles');
+            document.getElementById('favorites').style.display = 'none';
+            updateDOMWithMovieData(data);
+        });
+    });
+});
